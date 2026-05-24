166f25c8ef61d66b71be69671e019d80efafee82
Anuj Upadhayay <anujupadhayay0306@gmail.com>
2026-05-24 10:07:24 +0530

    Add ThemeProvider to main.jsx

--- frontend_/src/main.jsx
+++ frontend_/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
-import { ThemeProvider } from './context/ThemeContext'   // ✅ ADD THIS
+import { ThemeProvider } from './context/ThemeContext'   // ADD THIS
 import './index.css'
 import App from './App.jsx'
 
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <ThemeProvider>   {/* ✅ ADD THIS */}
+        <ThemeProvider>   {/* ADD THIS */}
           <App />
         </ThemeProvider>
       </AuthProvider>
